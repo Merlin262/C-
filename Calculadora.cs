4c82084f5ad6cb4b03c6ee07f23e3db08ce87d04
João Merlin <88944432+Merlin262@users.noreply.github.com>
2023-02-07 18:43:57 -0300

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula47
+{
+    internal class Calculadora
+    {
+        public static double Pi = 3.14;
+        
+        public static double Circunferencia(double r)
+        {
+            return (r * 2 * Pi);
+        }
+
+        public static double Volume(double r)
+        {
+            return 4 / 3 * Pi * r * r * r;
+        }
+    }
+}
